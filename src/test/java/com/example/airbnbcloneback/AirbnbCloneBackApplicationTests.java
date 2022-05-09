9f423731df94c006fe963f1267adbb5fd2c8840e
ekorda <ekorda@miu.edu>
2022-05-09 13:40:32 -0500

    fisrt commit

---
+++ src/test/java/com/example/airbnbcloneback/AirbnbCloneBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.airbnbcloneback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbnbCloneBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
